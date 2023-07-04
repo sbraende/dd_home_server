c892269e6eab363937e9f0055927d66187904d1e
sbraende <sebastian.braende@gmail.com>
2023-07-04 21:40:37 +0200

    Added class for db.

--- create.py
+++ create.py
@@ -2,8 +2,8 @@ import sqlite3
 import config
 
 def create_db(name):
-    con = sqlite3.connect(name)
+    con = sqlite3.connect(name, columns)
     cur = con.cursor()
-    cur.execute(f"CREATE TABLE {name}_table (datetime, user)")
+    cur.execute(f"CREATE TABLE {name}_table ({str(columns)})")
 
-
+create_db()
