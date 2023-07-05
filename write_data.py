70292b7ce9d5b7c3af9987e61f08eaf1ab01d85d
sbraende <sebastian.braende@gmail.com>
2023-07-05 21:30:48 +0200

    test file created

--- write_data.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def get_date_time():
-    return str(datetime.now())
-
-def write_data_to_db(room):
-    # Loop shit here...
-    # Can this even be looped? we need to know where we are logging...
-    #data = (datetime, room)
