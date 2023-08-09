864218952a450b937b5cac7d3eab84c01ed26eb5
Sebastian Braende <sebastian.braende@gmail.com>
2023-08-09 19:53:36 +0200

    Added whileloop to get data form get_weather()

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Server side digital-twin setup for Raspberry Pi 
 
-Using a Raspberry Pi with a DHT11, this program will log temperature and humidity data to a local sqlite database.
+Using a Raspberry Pi with a DHT11, this program will log temperature and humidity data to a local sqlite database. main.py will start the main server for logging data. app.py will open an api, so that you can get data from the raspberry. 
 
 Requires:
 - Raspberry Pi Raspberry OS connected with DHT11 sensor (hardcoded to GPIO datapin 17 at the moment).
@@ -11,4 +11,5 @@ Install:
 - In the config_db.py. Fill in the userdata fields on top of the document. Feel free to harcode the openweather_apikey or create a file in the project structure called "openweather_apikey.txt" and type in your personal api-key. You can get the api-key by regintering on https://openweathermap.org. 
 
 Run:
-- Start the program by running main.py. 
+- Start logging data to local sqlite3 database by running python3 main.py. 
+- Start api py running python3 app.py
--- server_functions.py
+++ server_functions.py
@@ -37,11 +37,17 @@ def get_weather(city):
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     api_key = config_db.userdata["openweather_apikey"]
     url = base_url + "appid=" + api_key + "&q=" + city
-    response = requests.get(url).json()
-    temprature = round(kelvin_to_celsius(response["main"]["temp"]), 2)
+
+    response = ""
+    while response == "":
+        response = requests.get(url).json()
+        if response == "":
+            time.sleep(120)
+
+    temperature = round(kelvin_to_celsius(response["main"]["temp"]), 2)
     humidity = response["main"]["humidity"]
     weather_main = response["weather"][0]["main"]
-    return temprature, humidity, weather_main
+    return temperature, humidity, weather_main
 
 
 def kelvin_to_celsius(kelvin):
@@ -66,15 +72,18 @@ def write_humidtemp_data(db_name, columns, room):
     sensor = Adafruit_DHT.DHT11
     pin = 17
 
-    countdown_duration = 120  # Dont put lower than 30 seconds
+    countdown_duration = 120  # No lower than 30 seconds. 600 ideal
 
     while True:
         end_time = time.time() + countdown_duration
+        
+        # Loopig to get data from sensor.
         last_temperature, last_humidity = 0.0, 0.0
         while time.time() < end_time:
             humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
             if humidity is not None and temperature is not None:
                 last_temperature, last_humidity = temperature, humidity
+        
         ext_temperature, ext_humidity, ext_weather = get_weather(
                                                     config_db.userdata["city"])
         data = (get_date_time(), room, last_temperature, last_humidity,
@@ -94,5 +103,3 @@ def print_db(db_name):
     connection, cursor = get_db(db_name)
     result = cursor.execute(f"SELECT * FROM {db_name}_table")
     print(result.fetchall())
-
-# print_db(config_db.humidtemp_db.name)
