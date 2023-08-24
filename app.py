36f72216a1a25e8d60a0b6016d411e494700b880
Sebastian Braende <sebastian.braende@gmail.com>
2023-08-24 10:54:23 +0200

    Made server class and function for data_rute()

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import json
 from flask import Flask, jsonify
 import Adafruit_DHT
 
-app = Flask(__name__)
 
 SENSOR_TYPES = {
     "DHT11": Adafruit_DHT.DHT11,
@@ -13,11 +12,14 @@ SENSOR_TYPES = {
 
 class Configuration():
     def __init__(self, config_path):
-        with open(config_path) as config_file:
-            self.config = json.load(config_file)
+        try: 
+            with open(config_path) as config_file:
+                self.config = json.load(config_file)
+        except json.JSONDecodeError as error:
+            print(f"Cant decode JSON {error}")
 
     def get_room(self):
-        return self.config["room"]
+        return self.room
 
     def get_host(self):
         return self.config["host"]
@@ -30,9 +32,6 @@ class Configuration():
 
     def get_sensor_pin(self):
         return self.config["sensor_pin"]
-
-
-config = Configuration("config.json")
 
 
 class Sensor():
@@ -50,19 +49,35 @@ class Sensor():
             raise ValueError(f"Unsupported sensor type: {self.sensor_type}")
 
 
+class Server:
+    def __init__(self) -> None:
+        self.config = Configuration("config.json")
+
+    def get_data(self):
+        sensor = Sensor(self.config.get_sensor_type(),
+                        self.config.get_sensor_pin())
+        humidity, temperature = sensor.read_data()
+        room = self.config.get_room()
+        data = {
+            "room": room,
+            "temperature": temperature,
+            "humidity": humidity,
+        }
+        return jsonify(data)
+
+
+app = Flask(__name__)
+
+
 @app.route("/data")
-def get_data():
-    sensor = Sensor(config.get_sensor_type(), config.get_sensor_pin())
-    humidity, temperature = sensor.read_data()
-    room = config.get_room()
-    data = {
-        "room": room,
-        "temperature": temperature,
-        "humidity": humidity,
-    }
-    return jsonify(data)
-    return data
+def data_rute():
+    server = Server()
+    return server.get_data()
+
+
+def main():
+    app.run(host="0.0.0.0", port=5000)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    main()
